853021cd810bc2cd6c773c1195b245aaa9c37ae0
Dan <dan.lawrence3@gmail.com>
2018-03-18 19:37:12 -0400

    create and read routes

--- controllers/athleteController.js
+++ controllers/athleteController.js
@@ -2,5 +2,24 @@ const express           = require ( 'express' );
 const athletes          = express.Router();
 const Athlete           = require ( '../models/athlete.js' );
 
+// INDEX
+athletes.get ( '/' , async ( req , res ) => {
+  try {
+    const allAthletes = await Athlete.find();
+    res.status( 200 ).json( allAthletes );
+  } catch ( error ) {
+    res.status( 400 ).json({error : err.message});
+  }
+});
+
+//CREATE
+athletes.post ( '/' , async ( req , res ) => {
+  try {
+    const newAthlete = await Athlete.create( req.body );
+    res.status( 200 ).json( newAthlete );
+  } catch ( error ) {
+    res.status( 400 ).json({error : err.message});
+  }
+});
 
 module.exports = athletes;
